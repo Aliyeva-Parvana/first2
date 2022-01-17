7e53833b5419c526e0bac093698ab81a76c70edc
Aliyeva-Parvana <alievvaparvana@gmail.com>
2022-01-17 19:17:52 +0400

    myprocetjs

---
+++ script.js
@@ -0,0 +1,22 @@
+const siyahi =["Paper.png", "das.png", "scissors.png"]
+
+//DOM Document Object Model
+ const  kagiz = document.getElementById("paper")
+ const das =document.getElementById("das")
+ const qayci = document.getElementById("qayci")
+
+ //komputerin qerari ve netice olan hisse
+ const komputerinSecimi =document.getElementById("komputer-secim")
+ const meticeniYaz = document.getElementById("netice")
+
+ //hadiseler onclict.onmustover,anchange
+ kagiz.onclick = seciminiEle
+ das.onclick =  seciminiEle
+ qayci.onclick = seciminiEle
+  function secimiEle(){
+
+  }
+  function randomSecim(){
+      
+  }
+
